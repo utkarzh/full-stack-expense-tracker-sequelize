487338f9949a59cd0420c6ac1a86877daa08ce42
utkarzh <113148455+utkarzh@users.noreply.github.com>
2023-08-03 04:39:37 +0530

    folders structured on frontend..

--- frontend/dashboard/dashboard.js
+++ frontend/dashboard/dashboard.js
@@ -3,8 +3,10 @@ const amount = document.getElementById("amount");
 const desc = document.getElementById("desc");
 const category = document.getElementById("category");
 const display = document.getElementById("res");
-const token=localStorage.getItem('token');
-axios.defaults.headers.common['Authorization'] = token;
+const premium = document.getElementById("premium");
+const token = localStorage.getItem("token");
+
+axios.defaults.headers.common["Authorization"] = token;
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
@@ -21,6 +23,42 @@ form.addEventListener("submit", async (e) => {
   }
 });
 
+premium.addEventListener("click", async () => {
+  try {
+    const resp = await axios.get("http://localhost:3000/buypremium");
+    const order = resp.data;
+    var options = {
+      key: order.key_id, // Replace with your Razorpay Key ID
+      amount: order.amount,
+      currency: order.currency,
+      order_id: order.id, // Use the order ID from the response
+      name: "Expense Tracker App",
+      description: "Premium Subscription",
+      handler: function (response) {
+        // Handle the payment success response here (optional)
+        console.log("Payment successful:", response);
+      },
+      prefill: {
+        email: "user@example.com",
+      },
+      notes: {},
+    };
+    const rzp = new Razorpay(options);
+    rzp.open();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const res = await axios.get("http://localhost:3000/expenses");
+
+    createItem(res.data.expenses);
+  } catch (error) {
+    alert("unable to fetch records!");
+  }
+});
 display.addEventListener("click", async (e) => {
   if (e.target.classList.contains("btn-danger")) {
     try {
@@ -33,16 +71,6 @@ display.addEventListener("click", async (e) => {
     }
   }
 });
-document.addEventListener("DOMContentLoaded", async () => {
-  try {
-    const res = await axios.get("http://localhost:3000/expenses");
-    
-   
-    createItem(res.data.expenses);
-  } catch (error) {
-    alert("unable to fetch records!");
-  }
-});
 
 function createItem(res) {
   console.log(res[0]);
@@ -50,22 +78,22 @@ function createItem(res) {
     const div = document.createElement("div");
     const hr = document.createElement("hr");
     const btn = document.createElement("button");
-    btn.classList = "btn btn-danger btn-sm mx-1 my-3";
+    btn.classList = "btn btn-danger btn-sm mx-4 my-3";
     btn.innerText = "Delete Expense";
     btn.setAttribute("id", item.id);
 
     div.classList = "container";
     div.innerHTML =
-      '<div class="row"><div class="col-auto"><u>AMOUNT:</u>  <b>' +
+      '<div class="row"><div class="col-auto"><li><u>AMOUNT:</u>  <b>' +
       item.amount +
       '</b></div><div class="col-auto"><u>CATEGORY:</u>  <b>' +
-      item.category +
+      item.category.toUpperCase() +
       '</b></div><div class="col-auto"><u>DESCRIPTION</u>:  <b>' +
-      item.desc +
-      "</b></div></>";
+      item.desc.toUpperCase() +
+      "</b></li></div></>";
     div.appendChild(btn);
     div.appendChild(hr);
 
-    display.appendChild(div);
+    display.prepend(div);
   });
 }
---
+++ frontend/signup/signup.js
@@ -0,0 +1,33 @@
+const form = document.getElementById("myform");
+const namee = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const number = document.getElementById("number");
+
+form.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  const confirmPassword = document.getElementById("cpassword").value;
+
+  if (password.value !== confirmPassword) {
+    alert("Passwords do not match. Please try again.");
+    return;
+  }
+  console.log;
+  const user = {
+    name: namee.value,
+    email: email.value,
+    password: password.value,
+    number: number.value,
+  };
+  try {
+    console.log(user);
+    var res = await axios.post("http://localhost:3000/signup", user);
+    console.log("success");
+    window.location.href = "login.html";
+  } catch (error) {
+    console.log(error);
+    alert(
+      "Signup failed might be because phone or email is already registered!"
+    );
+  }
+});
